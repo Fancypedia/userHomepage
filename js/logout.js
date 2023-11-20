16a77f6a2a8917b13d2adab9c4a015dda6980937
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-20 13:09:13 +0700

    [add] checkCookies

--- js/logout.js
+++
@@ -1,6 +0,0 @@
-import { deleteCookie } from "https://Fancypedia.github.io/js/cookies.js";
-
-export default function Logout() {
-    deleteCookie();
-    window.location.href = "https://fancypedia.my.id/";
-}
---
+++ js/mainLogout.js
@@ -0,0 +1,7 @@
+// import Logout from "./logout.js";
+// import checkCookie from "./checkCookie.js";
+import { Logout } from "https://Fancypedia.github.io/js/logout.js";
+import { checkCookie } from "https://Fancypedia.github.io/js/checkCookie.js";
+
+window.Logout = Logout;
+window.checkCookie = checkCookie;
