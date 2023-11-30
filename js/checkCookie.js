29fe2116f891f7c3d094a7b4884f5819f4a9bb74
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-30 11:06:55 +0700

    ganti return token jadi username

--- js/checkCookie.js
+++ js/checkCookie.js
@@ -1,9 +1,9 @@
 import { getCookie } from "./cookies.js";
 
 export default function checkCookie() {
-    let password = getCookie("password");
-	if (password != "") {
-	alert("Welcome to Fancypedia");
+    let username = getCookie("username");
+	if (username != "") {
+	alert("Hi " + username +" Welcome to Fancypedia");
 	} else {
 		alert("Biasakan Login Dulu Ya Bang!")
 
