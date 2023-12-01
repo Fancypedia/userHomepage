e2e0dc79ce6d4244b7aeebd23641e5a9aac026f1
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-12-01 23:53:47 +0700

    hapus semua file js

--- js/logout.js
+++
@@ -1,6 +0,0 @@
-import { deleteCookie } from "./cookies.js";
-
-export default function Logout() {
-    deleteCookie();
-    window.location.href = "https://fancypedia.my.id/pages/signin/index.html";
-}
--- js/mainLogout.js
+++
@@ -1,5 +0,0 @@
-import Logout from "./logout.js";
-import checkCookie from "./checkCookie.js";
-
-window.Logout = Logout;
-window.checkCookie = checkCookie;
